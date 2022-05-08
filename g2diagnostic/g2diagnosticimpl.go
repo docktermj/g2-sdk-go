625f7cc8baea3f5569725c43ac6c4ca219cfe45d
docktermj <michael@dockter.com>
2022-05-08 15:11:23 -0400

    #1 Documented trials

--- g2diagnostic/g2diagnosticimpl.go
+++ g2diagnostic/g2diagnosticimpl.go
@@ -76,9 +76,9 @@ func (g2diagnostic *G2diagnosticImpl) GetDBInfo(ctx context.Context) (string, er
 	// C.G2Diagnostic_getDBInfo(cStringBufferPointerPointer, &cStringBufferLength, (*[0]byte)(C.resizeStringBuffer))
 
 	// TRIAL:
-	cStringBufferPointer := (*C.char)(unsafe.Pointer(&stringBuffer[0]))
-	cStringBufferPointerPointer := (**C.char)(unsafe.Pointer(&cStringBufferPointer))
-	C.G2Diagnostic_getDBInfo(cStringBufferPointerPointer, &cStringBufferLength, (*[0]byte)(C.resizeStringBuffer))
+	cStringBufferPointer := (*C.char)(&stringBuffer)
+	// cStringBufferPointerPointer := (**C.char)(unsafe.Pointer(&cStringBufferPointer))
+	C.G2Diagnostic_getDBInfo(&cStringBufferPointer, &cStringBufferLength, (*[0]byte)(C.resizeStringBuffer))
 
 	return string(stringBuffer), err
 }
