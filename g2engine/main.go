b247527dfad7541a21c06b383c0bad5e119fba78
docktermj <michael@dockter.com>
2022-07-18 22:06:45 -0400

    #1 Flesh out interface

--- g2engine/main.go
+++ g2engine/main.go
@@ -33,11 +33,79 @@ type G2engineImpl struct{}
 type G2engine interface {
 	AddRecord(ctx context.Context, dataSourceCode string, recordID string, jsonData string, loadID string) error
 	AddRecordWithInfo(ctx context.Context, dataSourceCode string, recordID string, jsonData string, loadID string, flags int64) (string, error)
+	AddRecordWithInfoWithReturnedRecordID(ctx context.Context, dataSourceCode string, jsonData string, loadID string, flags int64, recordIDBuf string) (string, error)
+	AddRecordWithReturnedRecordID(ctx context.Context, dataSourceCode string, jsonData string, loadID string) (string, error)
+	CheckRecord(ctx context.Context, record string, recordQueryList string) (string, error)
 	ClearLastException(ctx context.Context) error
+	CloseExport(ctx context.Context, responseHandle interface{}) error
+	CountRedoRecords(ctx context.Context) (int64, error)
 	DeleteRecord(ctx context.Context, dataSourceCode string, recordID string, loadID string) error
 	DeleteRecordWithInfo(ctx context.Context, dataSourceCode string, recordID string, loadID string, flags int64) (string, error)
 	Destroy(ctx context.Context) error
+	ExportCSVEntityReport(ctx context.Context, csvColumnList string, flags int64) (interface{}, error)
+	ExportConfig(ctx context.Context) (string, error)
+	ExportConfigAndConfigID(ctx context.Context) (string, int64, error)
+	ExportJSONEntityReport(ctx context.Context, flags int64) (interface{}, error)
+	FetchNext(ctx context.Context, responseHandle interface{}) (string, error)
+	FindInterestingEntitiesByEntityID(ctx context.Context, entityID int64, flags int64) (string, error)
+	FindInterestingEntitiesByRecordID(ctx context.Context, dataSourceCode string, recordID string, flags int64) (string, error)
+	FindNetworkByEntityID(ctx context.Context, entityList string, maxDegree int, buildOutDegree int, maxEntities int) (string, error)
+	FindNetworkByEntityID_V2(ctx context.Context, entityList string, maxDegree int, buildOutDegree int, maxEntities int, flags int64) (string, error)
+	FindNetworkByRecordID(ctx context.Context, recordList string, maxDegree int, buildOutDegree int, maxEntities int) (string, error)
+	FindNetworkByRecordID_V2(ctx context.Context, recordList string, maxDegree int, buildOutDegree int, maxEntities int, flags int64) (string, error)
+	FindPathByEntityID(ctx context.Context, entityID1 int64, entityID2 int64, maxDegree int) (string, error)
+	FindPathByEntityID_V2(ctx context.Context, entityID1 int64, entityID2 int64, maxDegree int, flags int64) (string, error)
+	FindPathByRecordID(ctx context.Context, dataSourceCode1 string, recordID1 string, dataSourceCode2 string, recordID2 string, maxDegree int) (string, error)
+	FindPathByRecordID_V2(ctx context.Context, dataSourceCode1 string, recordID1 string, dataSourceCode2 string, recordID2 string, maxDegree int, flags int64) error
+	FindPathExcludingByEntityID(ctx context.Context, entityID1 int64, entityID2 int64, maxDegree int, excludedEntities string) (string, error)
+	FindPathExcludingByEntityID_V2(ctx context.Context, entityID1 int64, entityID2 int64, maxDegree int, excludedEntities string, flags int64) (string, error)
+	FindPathExcludingByRecordID(ctx context.Context, dataSourceCode1 string, recordID1 string, dataSourceCode2 string, recordID2 string, maxDegree int, excludedRecords string) (string, error)
+	FindPathExcludingByRecordID_V2(ctx context.Context, dataSourceCode1 string, recordID1 string, dataSourceCode2 string, recordID2 string, maxDegree int, excludedRecords string, flags int64) (string, error)
+	FindPathIncludingSourceByEntityID(ctx context.Context, entityID1 int64, entityID2 int64, maxDegree int, excludedEntities string) (string, error)
+	FindPathIncludingSourceByEntityID_V2(ctx context.Context, entityID1 int64, entityID2 int64, maxDegree int, excludedEntities string, requiredDsrcs string, flags int64) (string, error)
+	FindPathIncludingSourceByRecordID(ctx context.Context, dataSourceCode1 string, recordID1 string, dataSourceCode2 string, recordID2 string, maxDegree int, excludedRecords string, requiredDsrcs string) (string, error)
+	FindPathIncludingSourceByRecordID_V2(ctx context.Context, dataSourceCode1 string, recordID1 string, dataSourceCode2 string, recordID2 string, maxDegree int, excludedRecords string, requiredDsrcs string, flags int64) error
+	GetActiveConfigID(ctx context.Context) (int64, error)
+	GetEntityByEntityID(ctx context.Context, entityID int64) (string, error)
+	GetEntityByEntityID_V2(ctx context.Context, entityID int64, flags int64) (string, error)
+	GetEntityByRecordID(ctx context.Context, dataSourceCode string, recordID string) (string, error)
+	GetEntityByRecordID_V2(ctx context.Context, dataSourceCode string, recordID string, flags int64) (string, error)
 	GetLastException(ctx context.Context) (string, error)
+	GetLastExceptionCode(ctx context.Context) (int, error)
+	GetRecord(ctx context.Context, dataSourceCode string, recordID string) (string, error)
+	GetRecord_V2(ctx context.Context, dataSourceCode string, recordID string, flags int64) (string, error)
+	GetRedoRecord(ctx context.Context) (string, error)
+	GetRepositoryLastModifiedTime(ctx context.Context) (int64, error)
+	GetVirtualEntityByRecordID(ctx context.Context, recordList string) (string, error)
+	GetVirtualEntityByRecordID_V2(ctx context.Context, recordList string, flags int64) (string, error)
+	HowEntityByEntityID(ctx context.Context, entityID int64) (string, error)
+	HowEntityByEntityID_V2(ctx context.Context, entityID int64, flags int64) (string, error)
 	Init(ctx context.Context, moduleName string, iniParams string, verboseLogging int) error
+	InitWithConfigID(ctx context.Context, moduleName string, iniParams string, initConfigID int64, verboseLogging int) error
+	PrimeEngine(ctx context.Context) error
+	Process(ctx context.Context, record string) error
+	ProcessRedoRecord(ctx context.Context) (string, error)
+	ProcessRedoRecordWithInfo(ctx context.Context, flags int64) (string, string, error)
+	ProcessWithInfo(ctx context.Context, record string, flags int64) (string, error)
+	ProcessWithResponse(ctx context.Context, record string) (string, error)
+	ProcessWithResponseResize(ctx context.Context, record string) (string, error)
+	PurgeRepository(ctx context.Context) error
+	ReevaluateEntity(ctx context.Context, entityID int64, flags int64) error
+	ReevaluateEntityWithInfo(ctx context.Context, entityID int64, flags int64) (string, error)
+	ReevaluateRecord(ctx context.Context, dataSourceCode string, recordID string, flags int64) error
+	ReevaluateRecordWithInfo(ctx context.Context, dataSourceCode string, recordID string, flags int64) (string, error)
+	Reinit(ctx context.Context, initConfigID int64) error
+	ReplaceRecord(ctx context.Context, dataSourceCode string, recordID string, jsonData string, loadID string) error
+	ReplaceRecordWithInfo(ctx context.Context, dataSourceCode string, recordID string, jsonData string, loadID string, flags int64) (string, error)
+	SearchByAttributes(ctx context.Context, jsonData string) (string, error)
+	SearchByAttributes_V2(ctx context.Context, jsonData string, flags int64) (string, error)
 	Stats(ctx context.Context) (string, error)
+	WhyEntities(ctx context.Context, entityID1 int64, entityID2 int64) (string, error)
+	WhyEntities_V2(ctx context.Context, entityID1 int64, entityID2 int64, flags int64) (string, error)
+	WhyEntityByEntityID(ctx context.Context, entityID int64) (string, error)
+	WhyEntityByEntityID_V2(ctx context.Context, entityID int64, flags int64) (string, error)
+	WhyEntityByRecordID(ctx context.Context, dataSourceCode string, recordID string) (string, error)
+	WhyEntityByRecordID_V2(ctx context.Context, dataSourceCode string, recordID string, flags int64) (string, error)
+	WhyRecords(ctx context.Context, dataSourceCode1 string, recordID1 string, dataSourceCode2 string) (string error)
+	WhyRecords_V2(ctx context.Context, dataSourceCode1 string, recordID1 string, dataSourceCode2 string, flags int64) (string, error)
 }
